d1eaf4c37170675a1f0e1c1ca28036a3dc3d79a4
Joan Bruguera <joanbrugueram@gmail.com>
2019-11-14 21:29:29 +0100

    Spawn multiple slaves from the Docker test.

--- ddm-lmp/build.sh
+++ ddm-lmp/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+N_SLAVES=5
+
 sudo systemctl start docker || true # Make sure docker is up...
 sudo docker ps -a | grep -woP 'ddm-lmp-run.+' | sudo xargs -r docker rm -f
 
@@ -14,4 +16,8 @@ fi
 
 echo -e "\033[0;31m**Spawning master and slave DDM-LMP containers with IP 10.100.0.100 and 10.100.0.101 **\033[0m"
 sudo docker run --net ddm-lmp-net --ip "10.100.0.100" -d --name "ddm-lmp-run-master" ddm-lmp master -h 10.100.0.100
-sudo docker run --net ddm-lmp-net --ip "10.100.0.101" -d --name "ddm-lmp-run-slave" ddm-lmp slave -mh 10.100.0.100 -h 10.100.0.101
+
+for i in $(seq 1 $N_SLAVES); do
+	ip=$(( $i + 100))
+	sudo docker run --net ddm-lmp-net --ip "10.100.0.$ip" -d --name "ddm-lmp-run-slave$i" ddm-lmp slave -mh 10.100.0.100 -h "10.100.0.$ip"
+done
