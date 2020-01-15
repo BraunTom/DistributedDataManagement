59432cb68210c0fa3b2fb3412b2ead74130f9153
Joan Bruguera <joanbrugueram@gmail.com>
2020-01-15 17:30:24 +0100

    First working implementation with Spark of the IND discovery.

--- ind-discovery/src/main/scala/de/hpi/spark_tutorial/SimpleSpark.scala
+++ ind-discovery/src/main/scala/de/hpi/spark_tutorial/SimpleSpark.scala
@@ -5,7 +5,7 @@ import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.DecisionTreeClassificationModel
 import org.apache.spark.ml.classification.DecisionTreeClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.spark.ml.feature.{VectorIndexer}
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
@@ -20,6 +20,45 @@ class Pet(var name:String, var age:Int) {
 object SimpleSpark extends App {
 
   override def main(args: Array[String]): Unit = {
+    // Turn off logging
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Setting up a Spark Session
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Create a SparkSession to work with Spark
+    val sparkBuilder = SparkSession
+      .builder()
+      .appName("SparkTutorial")
+      .master("local[4]") // local, with 4 worker cores
+    val spark = sparkBuilder.getOrCreate()
+
+    // Set the default number of shuffle partitions (default is 200, which is too high for local deployment)
+    spark.conf.set("spark.sql.shuffle.partitions", "8") //
+
+    println("---------------------------------------------------------------------------------------------------------")
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Inclusion Dependency Discovery (Homework)
+    //------------------------------------------------------------------------------------------------------------------
+
+    val inputs = List("region", "nation", "supplier", "customer", "part", "lineitem", "orders")
+      .map(name => s"data/TPCH/tpch_$name.csv")
+
+    def time[R](block: => R): R = {
+      val t0 = System.currentTimeMillis()
+      val result = block
+      val t1 = System.currentTimeMillis()
+      println(s"Execution: ${t1 - t0} ms")
+      result
+    }
+
+    time {Sindy.discoverINDs(inputs, spark)}
+  }
+
+  def old_main(args: Array[String]): Unit = {
 
     // Turn off logging
     Logger.getLogger("org").setLevel(Level.OFF)
--- ind-discovery/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ ind-discovery/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,11 +1,125 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+    import spark.implicits._
 
-    // TODO
+    val tpchFlattenedTables = inputs.map(inputName => {
+      // Load the CSV file corresponding to each TPCH table into an RDD
+      val table = spark
+        .read
+        .option("inferSchema", "false")
+        .option("header", "true")
+        .option("quote", "\"")
+        .option("delimiter", ";")
+        .csv(inputName)
+        .toDF()
+
+      // "Flatten" the table so each cell is converted to a row along with its corresponding column name
+      // E.g. Before:
+      // |--------------|
+      // | COLA  | COLB |
+      // |--------------|
+      // | 1     | 2    |
+      // | 3     | 4    |
+      // |--------------|
+      // After:
+      // |--------------|
+      // | VALUE | COL  |
+      // |--------------|
+      // | 1     | COLA |
+      // | 2     | COLB |
+      // | 3     | COLA |
+      // | 4     | COLB |
+      // |--------------|
+      val columns = table.columns // TODO: Should broadcast?
+      table.flatMap(r => (0 until r.length).map(i => r.getString(i)).zip(columns))
+    })
+
+    // Concatenate all "flattened" tables of TPCH together
+    val tpchTuples = tpchFlattenedTables.reduce((a,b) => a.union(b))
+
+    // Calculate attribute sets: For each value, create a set containing the names of all the
+    // tables it appears in (after this step, the values are not longer important and are dropped)
+    // E.g. Before:
+    // |--------------|
+    // | VALUE | COL  |
+    // |--------------|
+    // | 1     | COLA |
+    // | 1     | COLA |
+    // | 1     | COLB |
+    // | 1     | COLC |
+    // | 2     | COLA |
+    // | 2     | COLC |
+    // | 3     | COLC |
+    // | 5     | COLC |
+    // |--------------|
+    // After:
+    // |----------------|
+    // | COLS           |
+    // |----------------|
+    // | COLA,COLB,COLC |
+    // | COLA,COLC      |
+    // | COLC           |
+    // | COLC           |
+    // |----------------|
+    val attributeSets = tpchTuples.groupByKey { case (value, _) => value }
+      .mapGroups { case (_, iterator) => iterator.map(x => x._2).toSet }
+
+    // Calculate inclusion lists: For each attribute set,
+    // create a tuple (value, attribute set - value)
+    // E.g. Before:
+    // |----------------|
+    // | COLS           |
+    // |----------------|
+    // | COLA,COLB,COLC |
+    // | COLA,COLC      |
+    // | COLC           |
+    // | COLC           |
+    // |----------------|
+    // After:
+    // |-------------------|
+    // | VALUE | COLS      |
+    // |-------------------|
+    // | COLA  | COLB,COLC |
+    // | COLB  | COLA,COLC |
+    // | COLC  | COLB,COLC |
+    // | COLA  | COLC      |
+    // | COLC  | COLA      |
+    // | COLC  | (Empty)   |
+    // | COLC  | (Empty)   |
+    // |-------------------|
+    val inclusionLists = attributeSets.flatMap(r => r.map(v => (v, r - v)))
+
+    // Calculate INDs: Group all inclusion lists by key, and intersect
+    // their corresponding column sets all together. Those are the INDs
+    // E.g. Before:
+    // |-------------------|
+    // | VALUE | COLS      |
+    // |-------------------|
+    // | COLA  | COLB,COLC |
+    // | COLB  | COLA,COLC |
+    // | COLC  | COLB,COLC |
+    // | COLA  | COLC      |
+    // | COLC  | COLA      |
+    // | COLC  | (Empty)   |
+    // | COLC  | (Empty)   |
+    // |-------------------|
+    // After:
+    // |-------------------|
+    // | VALUE | COLS      |
+    // |-------------------|
+    // | COLA  | COLC      |
+    // | COLB  | COLA,COLC |
+    // |-------------------|
+    val inds = inclusionLists.groupByKey { case(k, _) => k }
+        .mapGroups { case (k, sets) => (k, sets.map(a => a._2).reduce((a, b) => a.intersect(b))) }
+        .filter(r => r._2.nonEmpty)
+
+    // Collect and print the INDs (in order)
+    inds.collect().sortBy(x => x._1).foreach(x => println(x._1 + " < " + x._2.mkString(", ")))
   }
 }
